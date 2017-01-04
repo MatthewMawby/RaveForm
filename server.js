780a37f2943be467d464a16d7b084ec71d96341b
MatthewMawby <themesta@gmail.com>
2017-01-04 02:05:30 -0500

    reduced api calls to decrease latency

--- server.js
+++ server.js
@@ -19,15 +19,15 @@ var api;
 //initialization
 var init = function(bridge) {
     hostname = bridge[0]["ipaddress"];
-    username = "USERNAME";
+    username = "RVZO86tAOaakd1mfoAqLoKW0lvJlXr5Oqr1yT2ik";
     api = new HueApi(hostname, username);
 };
 hue.nupnpSearch().then(init).done();
 
 //create 3 lightStates, one for each light
-var state1 = lightState.create().transitiontime(0);
-var state2 = lightState.create().transitiontime(0);
-var state3 = lightState.create().transitiontime(0);
+var state1 = lightState.create().transitiontime(5);
+var state2 = lightState.create().transitiontime(5);
+var state3 = lightState.create().transitiontime(5);
 
 http.listen(PORT, function(){
     console.log("Server listening on: http://localhost:%s", PORT);
@@ -42,20 +42,19 @@ app.post('/hueData', function(req, res){
 
     var hue = req.body["hue"];
     var bri = req.body["brightness"];
-    if (hostname!="")
-    {
-    api.setLightState(1, state1.on(true).hsb(hue*360, 100, bri), function(err, lights) {
-             if (err) throw err;
-    });
+    if (hostname!=""){
+        api.setLightState(1, state1.hue(Math.round(hue*65535)), function(err, lights) {
+                 if (err) throw err;
+        });
 
-    api.setLightState(2, state1.on(true).hsb(hue*360, 100, bri), function(err, lights) {
-             if (err) throw err;
-    });
+        api.setLightState(2, state1.hue(Math.round(hue*65535)), function(err, lights) {
+                 if (err) throw err;
+        });
 
-    api.setLightState(3, state1.on(true).hsb(hue*360, 100, bri), function(err, lights) {
-             if (err) throw err;
-    });
-}
+        api.setLightState(3, state1.hue(Math.round(hue*65535)), function(err, lights) {
+                 if (err) throw err;
+        });
+    }
 
     res.sendStatus(200);
 });
